f0ce54ba0c63823e5c159e6e058967e44840b3eb
KostiantynLebediev <lebedievkostiantyn@gmail.com>
2024-10-14 17:10:36 +0100

    Home work done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import css from './App.module.css';
+import Description from '../Description/Descriptio';
+import Options from '../Options/Options';
+import Feedback from '../Feedback/Feedback';
+import Notification from '../Notification/Notification';
+import { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const initialState = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  
+  function App() {
+    const [feedback, setFeedback] = useState(() => {
+      const storagedFeedback = localStorage.getItem('feedback');
+  
+      if (storagedFeedback !== null) {
+        return JSON.parse(storagedFeedback);
+      }
+  
+      return initialState;
+    });
+  
+    const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+    const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
+  
+    useEffect(() => {
+      localStorage.setItem('feedback', JSON.stringify(feedback));
+    }, [feedback]);
+  
+    useEffect(() => {
+      document.title = totalFeedback
+        ? `Positive feedback ${positiveFeedback}%`
+        : 'No feedback yet';
+    }, [positiveFeedback, totalFeedback]);
+  
+    function updateFeedback(feedbackType) {
+      setFeedback({
+        ...feedback,
+        [feedbackType]: feedback[feedbackType] + 1,
+      });
+    }
+  
+    function resetFeedback() {
+      setFeedback(initialState);
+    }
+  
+    return (
+      <div className={css.container}>
+        <Description />
+        <Options
+          options={Object.keys(feedback)}
+          setter={updateFeedback}
+          total={totalFeedback}
+          reset={resetFeedback}
+        />
+        {totalFeedback ? (
+          <Feedback
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={totalFeedback}
+            positive={positiveFeedback}
+          />
+        ) : (
+          <Notification />
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    );
+  }
+  
+  export default App;
